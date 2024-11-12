5cecfcc1c18ae4556dc13fc091317838ec2240cf
User <User@DESKTOP-JO6NS6S>
2024-11-12 23:54:39 +0800

    n!

--- HW04/HW04.cpp
+++ HW04/HW04.cpp
@@ -1,40 +1,30 @@
 ﻿#include <iostream>
 using namespace std;
 
+int factorial(int n)
+{
+    if (n == 0) return 1;
+    else return n * factorial(n - 1);
+}
+
 int main()
 {
-	int n;
-	int sum = 0;
-	cout << "請輸入一個正整數n:";
-	cin >> n;
+    int num;
 
-	//for loop
-	for (int i = 1; i <= n; i++)
-	{
-		if (i % 2 == 0) sum += i;
-	}
+    cout << "請輸入一個正整數:";
+    cin >> num;
 
-	for (int i = 2; i <= n; i += 2)
-	{
-		sum += i;
-	}
-
-	//while
-	int i = 1;
-	sum = 0;
-	while (i <= n)
-	{
-		if (1 % 2 == 0)sum += i;
-		i++;
-	}
-
-	//do while
-	do {
-		sum = 0;
-		i += 2;
-	} while (i <= n);
-
-	cout << "1~" << n << "之間所有偶數的何為:" << sum << endl;
-
-
+    //非遞迴計算n!
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+        //result = result * i;
+    }
+    cout << "非遞迴版本:" << num << "! = " << result << endl;
+    //遞迴計算n!
+    result = factorial(num);
+    cout << "遞迴版本:" << num << "! = " << result << endl;
 }
+
+

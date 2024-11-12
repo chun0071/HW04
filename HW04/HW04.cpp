16f0dcad21efe9f91a047d64391fa6f29dd6955c
User <User@DESKTOP-JO6NS6S>
2024-11-13 00:10:56 +0800

    偶數和

--- HW04/HW04.cpp
+++ HW04/HW04.cpp
@@ -3,38 +3,41 @@ using namespace std;
 
 int main()
 {
-	int n;
-	int sum = 0;
-	cout << "請輸入一個正整數n:";
-	cin >> n;
+    int n = 1;
+    int sum = 0;
+    cout << "請輸入一個正整數n:";
+    cin >> n;
 
-	//for loop
-	for (int i = 1; i <= n; i++)
-	{
-		if (i % 2 == 0) sum += i;
-	}
+    //for loop
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0) sum += i;
+    }
 
-	for (int i = 2; i <= n; i += 2)
-	{
-		sum += i;
-	}
+    sum = 0;
+    for (int i = 2; i <= n; i += 2)
+    {
+        sum += i;
+    }
+    cout << "For loop: 1~" << n << "之間所有偶數的何為:" << sum << endl;
 
-	//while
-	int i = 1;
-	sum = 0;
-	while (i <= n)
-	{
-		if (1 % 2 == 0)sum += i;
-		i++;
-	}
+    //while loop
+    int i = 1;
+    sum = 0;
+    while (i <= n)
+    {
+        if (i % 2 == 0)sum += i;
+        i++;
+    }
+    cout << "While loop: 1~" << n << "之間所有偶數的何為:" << sum << endl;
 
-	//do while
-	do {
-		sum = 0;
-		i += 2;
-	} while (i <= n);
-
-	cout << "1~" << n << "之間所有偶數的何為:" << sum << endl;
-
+    //do while
+    i = 2;
+    sum = 0;
+    do {
+        sum += i;
+        i += 2;
+    } while (i <= n);
+    cout << "Do-While loop: 1~" << n << "之間所有偶數的何為:" << sum << endl;
 
 }
